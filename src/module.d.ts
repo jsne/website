7021ee68af27d7c7076c7dd1b772870a0613297d
nerdyman <averynerdyman@gmail.com>
2021-01-31 14:07:36 +0000

    feat: add type defs for SVGs

--- src/module.d.ts
+++ src/module.d.ts
@@ -1,2 +1,8 @@
 declare module '*.woff2';
 declare module '*.css';
+declare module '*.svg';
+
+declare module '~/assets/images/inline/*.svg' {
+  const Component: React.FC<React.SVGAttributes<SVGElement>>;
+  export default Component;
+}
