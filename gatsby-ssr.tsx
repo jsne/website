ed4b6ff28feb365627716928ce49e94cdb9ae876
nerdyman <averynerdyman@gmail.com>
2021-07-19 21:06:24 +0100

    build: update ssr config

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,15 +1,10 @@
-/* eslint-env node */
 import type { RenderBodyArgs } from 'gatsby';
 import * as React from 'react';
 
 import { getCssString } from './src/styles/stitches.config';
 
-export const onRenderBody = ({ setHeadComponents }: RenderBodyArgs): void => {
+export const onRenderBody = ({ setHeadComponents }: RenderBodyArgs) => {
   setHeadComponents([
-    <style
-      id="stitches"
-      key="stitches"
-      dangerouslySetInnerHTML={{ __html: getCssString() }}
-    />,
+    <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssString() }} />,
   ]);
 };
