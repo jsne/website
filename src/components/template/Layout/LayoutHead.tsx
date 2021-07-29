ad377ba4af6d30dd57987f8bb5fdc29922bcfd98
nerdyman <averynerdyman@gmail.com>
2021-07-29 20:26:39 +0100

    feat: set default html `lang`

--- src/components/template/Layout/LayoutHead.tsx
+++ src/components/template/Layout/LayoutHead.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Helmet, HelmetProps } from 'react-helmet-async';
+import { Helmet, HelmetProps, HtmlProps } from 'react-helmet-async';
 
 export type LayoutHeadProps = HelmetProps;
 
 /** `head` element with default global styles. */
-export const LayoutHead: React.FC<LayoutHeadProps> = (props) => (
-  <Helmet {...props}>
+export const LayoutHead: React.FC<LayoutHeadProps> = ({ htmlAttributes, ...props }) => (
+  <Helmet htmlAttributes={{ lang: 'en', ...htmlAttributes } as HtmlProps} {...props}>
     {!__IS_LIVE__ && <meta name="robots" content="noindex, nofollow" />}
   </Helmet>
 );
