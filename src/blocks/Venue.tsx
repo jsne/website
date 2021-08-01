85b6add2f67f0cb837ab86aa1b320cc242db9d64
nerdyman <averynerdyman@gmail.com>
2021-08-01 20:24:15 +0100

    types: add checks around venue properties in `Venue` block

--- src/blocks/Venue.tsx
+++ src/blocks/Venue.tsx
@@ -4,17 +4,28 @@ import { VenueSection, VenueSectionProps } from '~/components/compositions/Venue
 import { Mdx } from '~/components/template/Mdx';
 
 interface VenueProps {
-  venue: GatsbyTypes.VenueFragmentFragment;
+  venue: Required<GatsbyTypes.VenueFragmentFragment>;
 }
 
 /** Venue section with map, accepts and converts the venue fragment type. */
 export const Venue: React.FC<VenueProps> = ({ venue, ...props }) => {
+  if (
+    !venue.body?.childMdx?.body ||
+    !venue.name ||
+    !venue.address ||
+    !venue.mapsLink ||
+    !venue.location
+  ) {
+    console.error('[Venue] Some properties were missing', venue);
+    throw new Error('[Venue] Invalid venue provided');
+  }
+
   const parsedVenue: VenueSectionProps['venue'] = {
     preHeading: 'Venue',
-    heading: venue.name!,
-    postHeading: venue.address!,
-    body: <Mdx>{venue.body?.childMdx?.body!}</Mdx>,
-    cta: venue.mapsLink!,
+    heading: venue.name,
+    postHeading: venue.address,
+    body: <Mdx>{venue.body.childMdx.body}</Mdx>,
+    cta: venue.mapsLink,
   };
 
   return (
@@ -22,13 +33,13 @@ export const Venue: React.FC<VenueProps> = ({ venue, ...props }) => {
       id="event-venue"
       venue={parsedVenue}
       mapOptions={{
-        center: venue.location!,
+        center: venue.location,
         pitch: 30,
         bearing: -10,
         zoom: typeof window !== 'undefined' && window.innerWidth > 1280 ? 15.4 : 17,
       }}
       markerOptions={{
-        center: venue.location!,
+        center: venue.location,
       }}
       {...props}
     />
