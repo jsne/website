bf99826abf4d01fc3e46e394b940f535ae9f1b00
nerdyman <averynerdyman@gmail.com>
2022-07-04 11:53:32 +0100

    refactor: check for same page anchor first in `AutoLink` component

--- src/components/primitives/AutoLink.tsx
+++ src/components/primitives/AutoLink.tsx
@@ -17,13 +17,13 @@ export type AutoLinkProps = ScrollAnchorProps;
 const AutoLinkUnstyled = forwardRef<HTMLAnchorElement, AutoLinkProps>((props, ref) => {
   const { isInternal, isSamePage, isAnchor } = getLinkType(props.href);
 
-  if (isInternal && !isAnchor) {
-    // 'lil `as` hack, Gatsby types could be better.
-    return <Link ref={ref as unknown as string} {...props} to={props.href} />;
-  }
-
   if (isSamePage && isAnchor) {
     return <ScrollAnchor {...props} ref={ref} />;
+  }
+
+  if (isInternal) {
+    // 'lil `as` hack, Gatsby types could be better.
+    return <Link ref={ref as unknown as string} {...props} to={props.href} />;
   }
 
   return (
